de2749ce75ba1ff8ac4a32c7ab78279e73a2798a
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-11-24 20:22:59 +0530

    update vulnerable code

--- vulnerable_code_template.py
+++ vulnerable_code_template.py
@@ -1,17 +1,26 @@
-import marshal
+import json, pickle, hashlib, hmac, getpass
 import base64
 
-def foo():
-    pass # Your code here
+def fun(name: str,password: str):
+    s = {"username":name,"password":hashlib.md5(password.encode()).hexdigest()}
+    with open("users.json","w") as f:
+        signature = hmac.new(b'shared-public-key', pickle.dumps(s), hashlib.sha1).hexdigest()
+        s.update({'signature': signature})
+        f.write(json.dumps(s))
+    print('Added!')
 
-print ("""ctypes
-FunctionType
-(cmarshal
-loads
-(cbase64
-b64decode
-(S'%s'
-tRtRc__builtin__
-globals
-(tRS''
-tR(tR.""" % base64.b64encode(marshal.dumps(foo.func_code)))
+if __name__ == '__main__':
+    u = input("Username : ")
+    p = getpass.getpass()
+    fun(u,p)
+    print ("""ctypes
+    FunctionType
+    (cpickle
+    loads
+    (cbase64
+    b64decode
+    (S'%s'
+    tRtRc__builtin__
+    globals
+    (tRS''
+    tR(tR.""" % base64.b64encode(pickle.dumps(foo.func_code)))
