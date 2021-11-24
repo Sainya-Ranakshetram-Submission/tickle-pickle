07134796b3e9ead32ff0530556427c7cba22654e
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-11-24 16:21:13 +0530

    fixed the loophole

--- edited_code/client.py
+++ edited_code/client.py
@@ -1,13 +1,13 @@
-import os
-import pickle
-def fun(name,password):
-    s = {"username":name,"password":password}
-    safecode = pickle.dumps(s)
-    with open("users.json","wb") as f:
-        f.write(safecode)
-    return safecode
+import json, pickle, hashlib, hmac
+
+def fun(name: str,password: str) -> None:
+    s = {"username":name,"password":hashlib.md5(password.encode()).hexdigest()}
+    with open("users.json","w") as f:
+        signature = hmac.new(b'shared-public-key', pickle.dumps(s), hashlib.sha1).hexdigest()
+        s.update({'signature': signature})
+        f.write(json.dumps(s))
 
 if __name__ == '__main__':
     u = input("Username : ")
     p = input("Password : ")
-    yo_fun = fun(u,p)
+    fun(u,p)
--- edited_code/server.py
+++ edited_code/server.py
@@ -1,11 +1,15 @@
-import os
-import pickle
-def reverse_fun():
+import pickle, hmac, hashlib, json
+from typing import Union
+
+def reverse_fun() -> Union[str, dict]:
       with open("users.json","rb") as f:
-          data = f.read()
+            data = f.read()
+            json_data = json.loads(data)
+      if hmac.new(b'shared-public-key', pickle.dumps({"username":json_data["username"],"password":json_data["password"]}), hashlib.sha1).hexdigest() == json_data["signature"]:
+            return json_data
+      else:
+            return 'Integrity Error'
       
-      d = pickle.loads(data)
-      return d
 
 if __name__ == '__main__':
       print(reverse_fun())
